a1d616e67d91c22d4a77281552c44073df673b60
rahat-4 <mainul.rahat4@gmail.com>
2025-07-08 13:02:16 +0600

    Set allowed hosts to empty list

--- core/core/settings.py
+++ core/core/settings.py
@@ -14,10 +14,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config(
-    "ALLOWED_HOSTS", cast=lambda v: [s.strip() for s in v.split(",")]
-)
-
+ALLOWED_HOSTS = []
 
 # Application definition
 
